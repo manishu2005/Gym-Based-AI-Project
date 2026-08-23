0caa92463dd208007dc23212066d89dd59a63cd1
Manish Kumar <142162468+manishu2005@users.noreply.github.com>
2026-08-23 16:25:50 +0530

    Refactor LungesDetector for consistency and clarity

--- detectors/lunges.py
+++ detectors/lunges.py
@@ -1,4 +1,5 @@
-from core.base_excercise import BaseExercise
+from core.base_exercise import BaseExercise
+
 
 class LungesDetector(BaseExercise):
     DOWN_THRESHOLD = 100
@@ -6,55 +7,49 @@ class LungesDetector(BaseExercise):
     MIN_VISIBILITY = 0.7
     BALANCE_TOLERANCE = 0.10
 
-    LEFT_SHOULDER = 11
-    LEFT_ANKLE = 27
+    LEFT_HIP = 23
     LEFT_KNEE = 25
-    RIGHT_SHOULDER = 12
+    LEFT_ANKLE = 27
+    RIGHT_HIP = 24
     RIGHT_KNEE = 26
     RIGHT_ANKLE = 28
-    
-    LEFT_HIP = 23
-    RIGHT_HIP = 24
+    LEFT_SHOULDER = 11
+    RIGHT_SHOULDER = 12
 
     def __init__(self):
         super().__init__()
 
     def reset(self) -> None:
-        self.reps =0
-        self.stage =None
+        self.reps = 0
+        self.stage = None
 
-    
-    def process(self, landmarks)->dict:
+    def process(self, landmarks) -> dict:
         left_knee_angle = self.calculate_angle(
-            self.get_points(landmarks, self.LEFT_HIP),
-            self.get_points(landmarks, self.LEFT_KNEE),
-            self.get_points(landmarks, self.LEFT_ANKLE)
+            self.get_point(landmarks, self.LEFT_HIP),
+            self.get_point(landmarks, self.LEFT_KNEE),
+            self.get_point(landmarks, self.LEFT_ANKLE),
         )
 
         right_knee_angle = self.calculate_angle(
-            self.get_points(landmarks, self.RIGHT_HIP),
-            self.get_points(landmarks, self.RIGHT_KNEE),
-            self.get_points(landmarks, self.RIGHT_ANGLE)
+            self.get_point(landmarks, self.RIGHT_HIP),
+            self.get_point(landmarks, self.RIGHT_KNEE),
+            self.get_point(landmarks, self.RIGHT_ANKLE),
         )
 
         if left_knee_angle <= right_knee_angle:
             front_knee_angle = left_knee_angle
             front_hip_idx = self.LEFT_HIP
-            front_ankle_idx = self.LEFT_ANKLE
             front_knee_idx = self.LEFT_KNEE
+            front_ankle_idx = self.LEFT_ANKLE
             shoulder_idx_for_torso = self.LEFT_SHOULDER
         else:
             front_knee_angle = right_knee_angle
             front_hip_idx = self.RIGHT_HIP
-            front_ankle_idx = self.RIGHT_ANKLE
             front_knee_idx = self.RIGHT_KNEE
+            front_ankle_idx = self.RIGHT_ANKLE
             shoulder_idx_for_torso = self.RIGHT_SHOULDER
 
-        key_landmarks_visible = (
-            landmarks[front_hip_idx].visibility >= self.MIN_VISIBILITY and
-            landmarks[front_knee_idx].visibility >= self.MIN_VISIBILITY and
-            landmarks[front_ankle_idx].visibility >= self.MIN_VISIBILITY
-        )
+        key_landmarks_visible = landmarks[front_hip_idx].visibility > self.MIN_VISIBILITY and landmarks[front_knee_idx].visibility > self.MIN_VISIBILITY and landmarks[front_ankle_idx].visibility > self.MIN_VISIBILITY
 
         if key_landmarks_visible:
             if front_knee_angle < self.DOWN_THRESHOLD:
@@ -65,13 +60,12 @@ class LungesDetector(BaseExercise):
                 self.reps += 1
 
         torso_angle = self.calculate_angle(
-            self.get_points(landmarks, shoulder_idx_for_torso),
-            self.get_points(landmarks, front_hip_idx),
-            self.get_points(landmarks, front_knee_idx),
-
+            self.get_point(landmarks, shoulder_idx_for_torso),
+            self.get_point(landmarks, front_hip_idx),
+            self.get_point(landmarks, front_knee_idx),
         )
 
-        shoulder_mid_x = (landmarks[self.LEFT_SHOULDER].x + landmarks[self.RIGHT_SHOULDER].x) /2
+        shoulder_mid_x = (landmarks[self.LEFT_SHOULDER].x + landmarks[self.RIGHT_SHOULDER].x) / 2
         hip_mid_x = (landmarks[self.LEFT_HIP].x + landmarks[self.RIGHT_HIP].x) / 2
         lateral_offset = abs(shoulder_mid_x - hip_mid_x)
 
@@ -79,9 +73,11 @@ class LungesDetector(BaseExercise):
             balance_status = "BALANCED"
         else:
             balance_status = "OFF BALANCE"
-        return{
+
+        return {
             "reps": self.reps,
-            "front_knee_angle":int(front_knee_angle),
+            "front_knee_angle": int(front_knee_angle),
             "torso_angle": int(torso_angle),
-            "balance_status":balance_status,
+            "balance_status": balance_status,
         }
+    
